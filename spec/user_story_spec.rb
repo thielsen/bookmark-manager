99ff88803b5b12aa72ff2816c6cc0fbcace6733f
Simon Green <simon.green@thielsen.co.uk>
2019-01-28 13:50:55 +0000

    first commit

---
+++ spec/user_story_spec.rb
@@ -0,0 +1,22 @@
+# Show a list of bookmarks
+# Add new bookmarks
+# Delete bookmarks
+# Update bookmarks
+# Comment on bookmarks
+# Tag bookmarks into categories
+# Filter bookmarks by tag
+# Users manage their bookmarks
+
+# ```
+# As a user,
+# So I can look at the sites I have saved
+# I want to see a list of bookmarks
+# ```
+
+# Client    Controller    Model   View
+#  request->
+#                 request->
+#                 <-response
+#                     request ---->
+#                     <-----response
+#  <-bookmarks
